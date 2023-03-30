af0824b99718bd26fd0eba317cda3b3046c1aedc
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-03-31 00:32:03 +0100

    test: add test for case of multiple links with same URL

--- src/test/kotlin/dev/mcabsan/markdown/MarkdownTextShould.kt
+++ src/test/kotlin/dev/mcabsan/markdown/MarkdownTextShould.kt
@@ -16,7 +16,7 @@ class MarkdownTextShould {
     }
 
     @Test
-    fun `retrieve transformed content turning links into footnote`() {
+    fun `retrieve transformed content turning links into footnote anchors`() {
         val content = """
             # Test file
             
@@ -35,6 +35,41 @@ class MarkdownTextShould {
             es un librazo. ¡Cómpralo!
             
             [^anchor1]: https://www.codigosostenible.com
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `retrieve transformed content turning multiple links with same URL into footnote with shared anchors per URL`() {
+        val content = """
+            # Test file
+            
+            El libro de [Código Sostenible](https://www.codigosostenible.com)
+            es un librazo. ¡Cómpralo!
+            
+            De verdad, [Código Sostenible](https://www.codigosostenible.com) es un libro
+            muy recomendable.
+            
+            Buscadlo en [Savvily](https://www.savvily.es)
+        """.trimIndent()
+        val markdownText = MarkdownText(content)
+
+        val actual = markdownText.transform()
+
+        assertThat(actual).isEqualTo(
+            """
+            # Test file
+            
+            El libro de Código Sostenible[^anchor1]
+            es un librazo. ¡Cómpralo!
+            
+            De verdad, Código Sostenible[^anchor1] es un libro
+            muy recomendable.
+            
+            Buscadlo en Savvily[^anchor2]
+            
+            [^anchor1]: https://www.codigosostenible.com
+            [^anchor2]: https://www.savvily.es
             """.trimIndent()
         )
     }
