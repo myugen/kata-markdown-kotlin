e5faf63c524509de440f5dc38d01813028120a6e
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-03-31 00:18:14 +0100

    test: turn to green transformation

--- src/main/kotlin/dev/mcabsan/markdown/MarkdownText.kt
+++ src/main/kotlin/dev/mcabsan/markdown/MarkdownText.kt
@@ -6,11 +6,11 @@ data class MarkdownText(private val content: String) {
         var contentToTransform = content
         for ((index, link) in links.withIndex()) {
             val anchor = "[^anchor${index + 1}]"
-            val transformedContent = contentToTransform.replace(link.asMarkdownText(), "${link.label}$anchor")
+            val transformedContent = contentToTransform.replaceFirst(link.asMarkdownText(), "${link.label}$anchor")
             val footnoteDefinition = """
                 $anchor: ${link.url}
             """.trimIndent()
-            contentToTransform = "$transformedContent$footnoteDefinition"
+            contentToTransform = "$transformedContent\n\n$footnoteDefinition"
         }
 
         return contentToTransform
@@ -18,12 +18,14 @@ data class MarkdownText(private val content: String) {
 
     private fun findLinks(): List<Link> {
         // FIXME: This regex is not perfect, it does not support links with parenthesis
-        val linksRegex = Regex.fromLiteral("(\\[.+\\])(\\((https?)://.+\\))")
+        val linksRegex = "(\\[.+])(\\((https?)://.+\\))".toRegex()
         val matches = linksRegex.findAll(content)
         return matches.map { match ->
-            val label = match.groupValues[1].replace("[", "").replace("]", "")
-            val url = match.groupValues[2].replace("(", "").replace(")", "")
-            Link(label, url)
+            match.groups.size.takeIf { it >= 3 }.let {
+                val label = match.groups[1]!!.value.replace("[", "").replace("]", "")
+                val url = match.groups[2]!!.value.replace("(", "").replace(")", "")
+                Link(label, url)
+            }
         }.toList()
     }
 }
